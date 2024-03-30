a89b10c8367884e2a9fb2e9072eea0deb60194c3
codisho <codisho@users.noreply.github.com>
2024-03-30 18:58:17 +0530

    adding condition for new user creting restaurant

--- src/pages/ManageRestaurant.tsx
+++ src/pages/ManageRestaurant.tsx
@@ -13,22 +13,44 @@ export default function ManageRestaurant() {
   const isEditing = !!restaurant
 
   return (
-  <Tabs defaultValue="orders">
-    <TabsList>
-      <TabsTrigger value="orders">
-        Orders
-      </TabsTrigger>
-      <TabsTrigger value="manage-restaurant">
-        Manage Restaurant
-      </TabsTrigger>
-    </TabsList>
-    <TabsContent value="orders" className="space-y-5 bg-gray-50 p-10 rounded-lg">
-      <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
-      {orders?.map((order)=><OrderItemCard order={order}/>)}
-    </TabsContent>
-    <TabsContent value="manage-restaurant">
-    <ManageRestaurantForm restaurant={restaurant} onSave={isEditing?updateRestaurant:createRestaurant} isLoading={isCreateLoading||isUpdateLoading||isOrderLoading}/>
-    </TabsContent>
-  </Tabs>
-  )
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">
+          Orders
+        </TabsTrigger>
+        <TabsTrigger value="manage-restaurant">
+          Manage Restaurant
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="orders" className="space-y-5 bg-gray-50 p-10 rounded-lg">
+        {Array.isArray(orders) && orders.length > 0 ? (
+          <>
+            <h2 className="text-2xl font-bold">{orders.length} active orders</h2>
+            {orders.map((order, index) => (
+              <OrderItemCard key={index} order={order} />
+            ))}
+          </>
+        ) : (
+          <p>No orders available</p>
+        )}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        {restaurant !== null && restaurant !== undefined && (
+          <ManageRestaurantForm 
+            restaurant={restaurant} 
+            onSave={isEditing ? updateRestaurant : createRestaurant} 
+            isLoading={isCreateLoading || isUpdateLoading || isOrderLoading}
+          />
+        )}
+        {!restaurant && (
+          <ManageRestaurantForm 
+          restaurant={restaurant} 
+          onSave={isEditing ? updateRestaurant : createRestaurant} 
+          isLoading={isCreateLoading || isUpdateLoading || isOrderLoading}
+        />
+        )}
+      </TabsContent>
+    </Tabs>
+  );
+  
 }
